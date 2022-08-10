a85abf970c0341a996dc3be1c2c4c302e6b3241a
zewelor <zewelor@gmail.com>
2022-08-10 09:51:29 +0200

    Bump

--- .p10k.zsh
+++ .p10k.zsh
@@ -872,7 +872,7 @@
   typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=180
 
   # Context format when running with privileges: bold user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE=''
   # Context format when in SSH without privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
   # Default context format (no privileges, no SSH): user@hostname.
--- .zshrc
+++ .zshrc
@@ -365,13 +365,6 @@ if [ -n "$KONSOLE_DBUS_SERVICE" ]; then
   }
 fi
 
-# # Add alias only if asdf is installed
-# if [ -f "$HOME/.asdf/asdf.sh" ] ; then
-#   alias loadasdf='source ~/.asdf/asdf.sh'
-# elif [ -f "$HOME/.zinit/plugins/asdf-vm---asdf/asdf.sh" ] ; then
-#   alias loadasdf='source ~/.zinit/plugins/asdf-vm---asdf/asdf.sh'
-# fi
-
 function loadrails() {
   alias be='bundle exec'
   alias ror='bundle exec rails'
