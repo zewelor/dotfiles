7e32147876acfd4f2a343a3bd674f14b8dcf4783
zewelor <zewelor@gmail.com>
2023-03-11 20:14:12 +0100

    Enable only if docker

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -15,41 +15,42 @@ if has "docker"; then
   }
 
   zpcompdef _docker dkEsh='_docker_complete_containers_names'
+
+  if [ ! -z "`docker compose version`" ]; then
+
+    zpcompdef _docker-compose dkcrs="_docker-compose_services"
+
+    # zinit ice as"completion" ; zinit snippet https://github.com/docker/compose/blob/master/contrib/completion/zsh/_docker-compose
+
+    function dkcrs () {
+      dkc stop $1 && dkc up --force-recreate "$@[2,-1]" $1    
+    }
+
+    zpcompdef _docker-compose dkcrs="_docker-compose_services"
+
+    function dkcrsd () {
+      dkcrs $1 -d
+    }
+
+    zpcompdef _docker-compose dkcrsd="_docker-compose_services"
+
+    function dkcrsdl () {
+      dkcrsd $1 && dkcl -f $1
+    }
+
+    zpcompdef _docker-compose dkcrsdl="_docker-compose_services"
+
+    function dkcupdate () {
+      dkc stop $1 && dkc pull $1 && dkc up -d $1 && sleep 5 && dkcl -f $1
+    }
+
+    zpcompdef _docker-compose dkcupdate="_docker-compose_services"
+
+    function dkcupdated () {
+      dkc stop $1 && dkc pull $1 && dkc up -d $1
+    }
+
+    zpcompdef _docker-compose dkcupdated="_docker-compose_services"
+  fi
 fi
 
-if [ ! -z "`docker compose version`" ]; then
-
-  zpcompdef _docker-compose dkcrs="_docker-compose_services"
-
-  # zinit ice as"completion" ; zinit snippet https://github.com/docker/compose/blob/master/contrib/completion/zsh/_docker-compose
-
-  function dkcrs () {
-    dkc stop $1 && dkc up --force-recreate "$@[2,-1]" $1    
-  }
-
-  zpcompdef _docker-compose dkcrs="_docker-compose_services"
-
-  function dkcrsd () {
-    dkcrs $1 -d
-  }
-
-  zpcompdef _docker-compose dkcrsd="_docker-compose_services"
-
-  function dkcrsdl () {
-    dkcrsd $1 && dkcl -f $1
-  }
-
-  zpcompdef _docker-compose dkcrsdl="_docker-compose_services"
-
-  function dkcupdate () {
-    dkc stop $1 && dkc pull $1 && dkc up -d $1 && sleep 5 && dkcl -f $1
-  }
-
-  zpcompdef _docker-compose dkcupdate="_docker-compose_services"
-
-  function dkcupdated () {
-    dkc stop $1 && dkc pull $1 && dkc up -d $1
-  }
-
-  zpcompdef _docker-compose dkcupdated="_docker-compose_services"
-fi
