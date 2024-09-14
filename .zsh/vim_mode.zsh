09ac1a86ae9af92ba2bdd2a3223103f8796e679d
zewelor <zewelor@gmail.com>
2024-09-14 11:55:13 +0200

    Refactor vim_mode.zsh and install script
     • Updated vim_mode.zsh to use zinit for loading zsh-vi-mode.
     • Removed redundant initialization of ZVM_INIT_MODE.
     • Commented out error trap in install script.
     • Added function to configure Git conflict style based on version.
     • Implemented logic to set conflict style in ~/.gitconfig.local.
     • Ensured secret environment variables are added before configuring Git.

--- .zsh/vim_mode.zsh
+++ .zsh/vim_mode.zsh
@@ -1,9 +1,11 @@
 # https://github.com/jeffreytse/zsh-vi-mode
-ZVM_INIT_MODE=sourcing
+
+# https://github.com/BilderLoong/dotfiles/blob/e75daa3234b8b8a2bd4008d71f5d9de104ed6c58/zsh/zsh/custom/zinit.sh#L28
+zinit light-mode for \
+  atinit"
+    ZVM_INIT_MODE=sourcing
+  "\
+  jeffreytse/zsh-vi-mode
+
 ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
-
-zinit ice depth=1
-zinit light jeffreytse/zsh-vi-mode
-
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
-# ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
