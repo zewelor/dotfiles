9b9ae05a5333ba775ced504237d9ff3d2e6d8727
zewelor <zewelor@gmail.com>
2024-11-03 08:46:57 +0000

    Add functions to set Konsole tab title; remove set_konsole_title.zsh
    - Introduced `set-konsole-tab-title-type` and `set-konsole-tab-title` functions in `kde.zsh` for managing Konsole tab titles.
    - Removed the `set_konsole_title.zsh` file as its functionality is now integrated into `kde.zsh`.

--- .zsh/kde.zsh
+++ .zsh/kde.zsh
@@ -27,4 +27,21 @@ if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]]; then
           echo "Error: toggle_screen_lock.sh not found or not executable."
       fi
   }
+
+  if [ -n "$KONSOLE_DBUS_SERVICE" ]; then
+    set-konsole-tab-title-type ()
+    {
+        local _title=$1
+        local _type=${2:-0}
+        [[ -z "${_title}" ]]               && return 1
+        [[ -z "${KONSOLE_DBUS_SERVICE}" ]] && return 1
+        [[ -z "${KONSOLE_DBUS_SESSION}" ]] && return 1
+        qdbus >/dev/null "${KONSOLE_DBUS_SERVICE}" "${KONSOLE_DBUS_SESSION}" setTabTitleFormat "${_type}" "${_title}"
+    }
+
+    set-konsole-tab-title ()
+    {
+        set-konsole-tab-title-type $1 && set-konsole-tab-title-type $1 1
+    }
+  fi
 fi
