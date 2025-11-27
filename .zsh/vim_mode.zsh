3d8392ae1397480eca1c0eba67537d303bc292ef
zewelor <zewelor@gmail.com>
2025-11-27 11:30:37 +0000

    Feat: Rebind atuin keys after zsh-vi-mode initialization
    The `zsh-vi-mode` plugin overrides custom keybindings, so this introduces a `zvm_after_init` function to re-bind essential atuin keybindings (`^r`, `up arrow`, and `k` in command mode) to restore full atuin functionality.

--- .zsh/vim_mode.zsh
+++ .zsh/vim_mode.zsh
@@ -10,3 +10,14 @@ zinit light-mode wait lucid for \
 ZVM_LAZY_KEYBINDINGS=false
 ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
+
+function zvm_after_init() {
+  # Re-bind atuin keys as zsh-vi-mode overrides them
+  if zle -l atuin-search; then
+    bindkey -M viins '^r' atuin-search-viins
+    bindkey -M vicmd '^r' atuin-search-vicmd
+    bindkey -M viins '^[[A' atuin-up-search-viins
+    bindkey -M vicmd '^[[A' atuin-up-search-vicmd
+    bindkey -M vicmd 'k' atuin-up-search-vicmd
+  fi
+}
