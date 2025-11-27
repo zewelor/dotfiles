0bfff9281528e7117152208ba391354018a98feb
zewelor <zewelor@gmail.com>
2025-11-27 19:32:05 +0000

    feat(zsh): Enhance Vi mode Atuin integration and fix completion Esc key
    - Enable Atuin history search for Up arrow keys and bind 'k' in vicmd mode for standard navigation.
    - Removed `--disable-up-arrow` flag during Atuin initialization to facilitate bindings.
    - Fix Esc key functionality in zsh `menuselect` mode (completion lists) by binding '^[' to send-break.

--- .zsh/vim_mode.zsh
+++ .zsh/vim_mode.zsh
@@ -12,6 +12,10 @@ ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
 
 function zvm_after_init() {
+  # Fix Esc in menu selection (autocomplete list)
+  zmodload zsh/complist
+  bindkey -M menuselect '^[' send-break
+
   # Only run if Atuin's vi widgets exist (atuin >= 18)
   (( $+widgets[atuin-search-viins] && $+widgets[atuin-search-vicmd] )) || return
 
@@ -19,15 +23,17 @@ function zvm_after_init() {
   zvm_bindkey viins '^R' atuin-search-viins
   zvm_bindkey vicmd '^R' atuin-search-vicmd
 
-  # Up arrow (terminfo first, then common fallbacks)
-  # local up="${terminfo[kcuu1]}"
-  # [[ -n "$up" ]] && {
-  #   zvm_bindkey viins "$up" atuin-up-search-viins
-  #   zvm_bindkey vicmd "$up" atuin-up-search-vicmd
-  # }
+  local up="${terminfo[kcuu1]}"
+  [[ -n "$up" ]] && {
+    zvm_bindkey viins "$up" atuin-up-search-viins
+    zvm_bindkey vicmd "$up" atuin-up-search-vicmd
+  }
 
-  # zvm_bindkey viins '^[[A' atuin-up-search-viins
-  # zvm_bindkey viins '^[OA'  atuin-up-search-viins
-  # zvm_bindkey vicmd '^[[A' atuin-up-search-vicmd
-  # zvm_bindkey vicmd '^[OA'  atuin-up-search-vicmd
+  zvm_bindkey viins '^[[A' atuin-up-search-viins
+  zvm_bindkey viins '^[OA'  atuin-up-search-viins
+  zvm_bindkey vicmd '^[[A' atuin-up-search-vicmd
+  zvm_bindkey vicmd '^[OA'  atuin-up-search-vicmd
+  
+  # Bind 'k' in normal mode to Atuin up-search (matches Atuin defaults)
+  zvm_bindkey vicmd 'k' atuin-up-search-vicmd
 }
--- .zshrc
+++ .zshrc
@@ -657,7 +657,7 @@ done
 
 if is_desktop && is-at-least '2.32' `getconf GNU_LIBC_VERSION | rev | cut -d " " -f 1 | rev` ; then
   zinit light-mode as"program" from"gh-r" bpick"atuin-*.tar.gz" mv"atuin*/atuin -> atuin" \
-    atclone"./atuin init zsh --disable-up-arrow > init.zsh; ./atuin gen-completions --shell zsh > _atuin" \
+    atclone"./atuin init zsh > init.zsh; ./atuin gen-completions --shell zsh > _atuin" \
     atpull"%atclone" src"init.zsh" for @atuinsh/atuin
 else
   zinit light-mode for joshskidmore/zsh-fzf-history-search
