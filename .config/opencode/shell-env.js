14e66e8f9113ded9b6342cea26e6699ac2694a03
zewelor <zewelor@gmail.com>
2026-06-12 10:04:59 +0100

    style: inline opencode shell env defaults

--- .config/opencode/shell-env.js
+++ .config/opencode/shell-env.js
@@ -1,11 +1,9 @@
-const shellEnv = {
-  GIT_PAGER: "cat",
-}
-
 export const ShellEnvPlugin = async () => {
   return {
     "shell.env": async (_input, output) => {
-      Object.assign(output.env, shellEnv)
+      Object.assign(output.env, {
+        GIT_PAGER: "/usr/bin/cat",
+      })
     },
   }
 }
